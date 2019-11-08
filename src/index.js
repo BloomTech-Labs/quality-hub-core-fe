04f2076c3eefeceab94267a5ffa9a320b29b12a2
Jonathan Ho <johnnyho82@gmail.com>
2019-11-08 10:16:01 -0500

    Update uri with staging GraphQL endpoint

--- src/index.js
+++ src/index.js
@@ -2,31 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from 'react-router-dom';
 import ApolloClient from 'apollo-boost';
-import {ApolloProvider} from '@apollo/react-hooks';
+import { ApolloProvider } from '@apollo/react-hooks';
 
-const getToken = () =>{
-    let token = localStorage.getItem('token');
-    return token;
-}
+const getToken = () => {
+  let token = localStorage.getItem('token');
+  return token;
+};
 const client = new ApolloClient({
-    uri: 'https://quality-hub-gateway.herokuapp.com/',
-    request: (operation) =>{
-        operation.setContext({
-            headers: {
-                authorization: getToken(),
-            },
-        });
-    }
+  uri: 'https://quality-hub-gateway-staging.herokuapp.com/',
+  request: operation => {
+    operation.setContext({
+      headers: {
+        authorization: getToken(),
+      },
+    });
+  },
 });
 
-
-
 ReactDOM.render(
-    <ApolloProvider client={client}>
+  <ApolloProvider client={client}>
     <Router>
-        <App />
+      <App />
     </Router>
-    </ApolloProvider>
-    , document.getElementById('root'));
+  </ApolloProvider>,
+  document.getElementById('root'),
+);
