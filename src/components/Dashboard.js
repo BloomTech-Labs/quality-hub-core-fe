c9492fa5dea86d25e871e79d83057fe94925856d
Jonathan Ho <johnnyho82@gmail.com>
2019-11-08 10:24:15 -0500

    Comment out token in GET_USER definition

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,166 +1,172 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { gql } from 'apollo-boost';
 import { useMutation } from '@apollo/react-hooks';
-import {useQuery, useLazyQuery} from '@apollo/react-hooks';
-
+import { useQuery, useLazyQuery } from '@apollo/react-hooks';
 
 const EDIT_USER = gql`
-mutation update(
-    $first_name: String!,
-    $last_name: String!, 
-    $email: String!, 
-    $password: String!, 
-    $city: String!, 
-    $state: String!,
-    # $image: String,
-    # $gender: String,
-    # $personal_url: String,
-    # $blog_url: String,
-    # $linkedin_url: String,
-    # $github_url: String,
-    # $bio: String,
-    ){
-        update(
-            first_name: $first_name,
-            last_name: $last_name,
-            email: $email,
-            password: $password,
-            city: $city,
-            state: $state,
-            # image: $image,
-            # gender: $gender,
-            # personal_url: $personal_url,
-            # blog_url: $blog_url,
-            # linkedin_url: $linkedin_url,
-            # github_url: $github_url,
-            # bio: $bio
-        ) {
-            token,
-            user{
-                id
-            }
-        }
-    }`
-
+  mutation update(
+    $first_name: String!
+    $last_name: String!
+    $email: String!
+    $password: String!
+    $city: String!
+    $state: String! # $image: String, # $gender: String,
+  ) # $personal_url: String,
+  # $blog_url: String,
+  # $linkedin_url: String,
+  # $github_url: String,
+  # $bio: String,
+  {
+    update(
+      first_name: $first_name
+      last_name: $last_name
+      email: $email
+      password: $password
+      city: $city
+      state: $state # image: $image, # gender: $gender,
+    ) # personal_url: $personal_url,
+    # blog_url: $blog_url,
+    # linkedin_url: $linkedin_url,
+    # github_url: $github_url,
+    # bio: $bio
+    {
+      token
+      user {
+        id
+      }
+    }
+  }
+`;
 
 const GET_USER = gql`
-    query {
-        me {
-            id
-            first_name
-            last_name
-            email
-            city
-            state
-            # image
-            # gender
-            # personal_url
-            # blog_url
-            # linkedin_url
-            # github_url
-            # bio  
-        }
-        {
-            token
-        }
-    } 
-` 
-const Dashboard = (props) => {
-    const userID = {
-        id: null
-    };
+  query {
+    me {
+      id
+      first_name
+      last_name
+      email
+      city
+      state
+      # image
+      # gender
+      # personal_url
+      # blog_url
+      # linkedin_url
+      # github_url
+      # bio
+    }
+    # {
+    #     token
+    # }
+  }
+`;
+const Dashboard = props => {
+  const userID = {
+    id: null,
+  };
 
-    const [user, userMutation] = useMutation(EDIT_USER);
-    const [getUser, {data: userData}] = useLazyQuery(GET_USER);
-    const [editUser, setEditUser] = useState(userData);
-    const [editing, setEditing] = useState(false);
-    const [testEditingValue, setTestEditingValue] = useState({
-        testname: "Julie A"
+  const [user, userMutation] = useMutation(EDIT_USER);
+  const [getUser, { data: userData }] = useLazyQuery(GET_USER);
+  const [editUser, setEditUser] = useState(userData);
+  const [editing, setEditing] = useState(false);
+  const [testEditingValue, setTestEditingValue] = useState({
+    testname: 'Julie A',
+  });
+  const [testOriginalName, setTestOriginalName] = useState('Julie A');
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      userID.id = localStorage.getItem('id');
+      getUser();
+    }
+  }, []);
+
+  const [userInfo, editUserInfo] = useState({
+    first_name: '',
+    last_name: '',
+    password: '',
+    email: '',
+    city: '',
+    state: '',
+    // image: "",
+    // gender: "",
+    // personal_url: "",
+    // blog_url: "",
+    // linkedin_url: "",
+    // github_url: "",
+    // bio: ""
+  });
+
+  const handleChange = e => {
+    setEditUser({
+      ...editUser,
+      [e.target.name]: e.target.value,
     });
-    const [testOriginalName, setTestOriginalName]= useState("Julie A")
+  };
+  const handleTestChange = e => {
+    setTestEditingValue({
+      ...testEditingValue,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-    useEffect(()=>{
-        const token = localStorage.getItem('token');
-        if(token){
-            userID.id = localStorage.getItem('id');
-            getUser();
-        }
-    }, [])
+  const handleTestCancelEdit = e => {
+    setEditing(false);
+    setTestEditingValue({
+      ...testEditingValue,
+      testname: testOriginalName,
+    });
+  };
 
-    const [userInfo, editUserInfo] = useState({
-        first_name: "",
-        last_name: "",
-        password: "",
-        email: "",
-        city: "",
-        state: "",
-        // image: "",
-        // gender: "",
-        // personal_url: "",
-        // blog_url: "",
-        // linkedin_url: "",
-        // github_url: "",
-        // bio: ""
-    })
+  const acceptTestEdit = e => {
+    console.log('accept');
+    console.log(testOriginalName);
+    setTestOriginalName(testEditingValue.testname);
+    console.log(testOriginalName);
+    setEditing(false);
+  };
 
-   
+  const handleSubmit = e => {
+    e.preventDefault();
+    user({ variables: user })
+      .then(results => {
+        console.log(results);
+        let token = localStorage.token;
+        //   loginStatus.error = null;
+        props.history.push('/dashboard');
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    console.log(user);
+  };
+  return (
+    <div className='editform'>
+      {editing ? (
+        <input
+          name='testname'
+          value={testEditingValue.testname}
+          onChange={handleTestChange}
+        />
+      ) : (
+        testOriginalName
+      )}
+      {editing && (
+        <button
+          onClick={() => handleTestCancelEdit()}
+          className='cancel-button'>
+          X
+        </button>
+      )}
+      {editing && (
+        <button onClick={() => acceptTestEdit()} className='accept-button'>
+          &#x2713;
+        </button>
+      )}
+      {!editing && <button onClick={() => setEditing(true)}>EDIT</button>}
 
-    
-    const handleChange = (e) => {
-
-        setEditUser({
-            ...editUser,
-            [e.target.name]: e.target.value
-        })
-    }
-    const handleTestChange = e =>{
-        setTestEditingValue({
-            ...testEditingValue,
-            [e.target.name]:e.target.value
-        })
-    }
-
-    const handleTestCancelEdit = e =>{
-        setEditing(false);
-        setTestEditingValue({
-            ...testEditingValue,
-            testname: testOriginalName
-        })
-    }
-
-    const acceptTestEdit = e =>{
-        console.log('accept');
-        console.log(testOriginalName);
-        setTestOriginalName(testEditingValue.testname);
-        console.log(testOriginalName);
-        setEditing(false);
-        
-    }
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        user({variables: user})
-          .then(results => {
-              console.log(results);
-              let token = localStorage.token;
-            //   loginStatus.error = null;
-              props.history.push('/dashboard')
-          })
-          .catch(err=>{
-              console.log(err);
-          })
-          console.log(user);
-      }
-    return (
-        <div className="editform">
-            {editing ? <input name="testname" value={testEditingValue.testname} onChange={handleTestChange} /> : testOriginalName}
-            {editing && <button onClick={()=> handleTestCancelEdit()} className="cancel-button">X</button>}
-            {editing && <button onClick={()=> acceptTestEdit()} className="accept-button">&#x2713;</button>}
-            {!editing && <button onClick = {() => setEditing(true)}>EDIT</button>}
-
-
-
-            {/* <form onSubmit={handleSubmit}>
+      {/* <form onSubmit={handleSubmit}>
             <input
             name="first_name"
             placeholder= {userData.first_name}
@@ -193,8 +199,8 @@ const Dashboard = (props) => {
             />
             <button type='submit'>Submit </button>
             </form> */}
-        </div>
-    );
-}
+    </div>
+  );
+};
 
 export default Dashboard;
