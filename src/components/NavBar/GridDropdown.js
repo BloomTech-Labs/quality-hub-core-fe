eb871564f1000417ab075c43f57985d5f7598907
Krishan Nattar <krishan.nattar@gmail.com>
2019-11-11 12:58:40 -0800

    grid dropdown working!!!!

--- src/components/NavBar/GridDropdown.js
+++ src/components/NavBar/GridDropdown.js
@@ -1,69 +1,63 @@
-import React, {useEffect, useState, useRef} from 'react';
+import React, { useEffect, useState, useRef } from "react";
 
-const GridDropdown = (props) => {
+const GridDropdown = props => {
+  const node = useRef();
+  const [open, setOpen] = useState(false);
 
-    const node = useRef();
-    const [open, setOpen] = useState(false);
-
-    const handleOutsideClick = e =>{
-        console.log('clicked outside');
-        if(node.current.contains(e.target)) {
-            return;
-        }
-        setOpen(false);
+  const handleOutsideClick = e => {
+    console.log("clicked outside");
+    if (node.current.contains(e.target)) {
+      return;
     }
+    setOpen(false);
+  };
 
-    useEffect(()=>{
-        if(open){
-            document.addEventListener("mousedown", handleOutsideClick);
-        } else {
-            document.removeEventListener("mousedown", handleOutsideClick);
-        }
-    }, [open]);
+  useEffect(() => {
+    if (open) {
+      document.addEventListener("mousedown", handleOutsideClick);
+    } else {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    }
+  }, [open]);
 
-    return ( 
-        <div ref={node}>
-            <img
-            src="grid.svg"
-            alt="Grid Menu"
-            className="grid-menu"
-            onClick={e=>setOpen(!open)}
-          />
-          {open && (<p>Hello</p>)}
-        </div>
-     );
-}
- 
-export default GridDropdown;
-
-
-{/* <div className="dropdown grid-icon"> */}
-          
-          
-          {/* <div className="dropdown-grid-content hidden dropdown-icons">
-            <div>
+  return (
+    <div ref={node}>
+      <img
+        src="grid.svg"
+        alt="Grid Menu"
+        className="grid-menu grid-icon"
+        onClick={e => setOpen(!open)}
+      />
+      {open && (
+        <div className="dropdown-grid-content dropdown-icons">
+          <div>
             <a href="#">
-              <img src="favicon.svg" height="50px" width="50px"/>
+              <img src="favicon.svg" height="50px" width="50px" />
               <p>Resume Q</p>
-              </a>
-            </div>
-            <div>
-            <a href="#">
-            <img src="sad.svg" height="50px" width="50px"/>
-            <p>Interview Q</p>
             </a>
-            </div>
-            <div>
-            <a href="#">
-            <img src="quailr.svg" height="50px" width="50px"/>
-            <p>QuailR</p>
-            </a>
-            </div>
-            <div>
-            <a href="#">
-            <img src="drug.svg" height="50px" width="50px"/>
-            <p>Design Q</p>
-            </a>
-            </div>
           </div>
-        </div> */}
+          <div>
+            <a href="#">
+              <img src="sad.svg" height="50px" width="50px" />
+              <p>Interview Q</p>
+            </a>
+          </div>
+          <div>
+            <a href="#">
+              <img src="quailr.svg" height="50px" width="50px" />
+              <p>QuailR</p>
+            </a>
+          </div>
+          <div>
+            <a href="#">
+              <img src="drug.svg" height="50px" width="50px" />
+              <p>Design Q</p>
+            </a>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default GridDropdown;
