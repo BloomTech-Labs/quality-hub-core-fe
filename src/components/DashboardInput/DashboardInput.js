c8bc59ae8b95ab4684cca4f01d9ec36e641637f8
Krishan Nattar <krishan.nattar@gmail.com>
2019-11-12 21:12:35 -0800

    state dropdown list working

--- src/components/DashboardInput/DashboardInput.js
+++ src/components/DashboardInput/DashboardInput.js
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import { gql } from 'apollo-boost';
-import { useMutation } from '@apollo/react-hooks';
-import './DashboardInput.scss';
+import React, { useState, useEffect } from "react";
+import { gql } from "apollo-boost";
+import { useMutation } from "@apollo/react-hooks";
+import "./DashboardInput.scss";
+import {statesArray} from '../SignUpForm/States';
 
-import { capitalize } from '../../utils/capitalize';
+import { capitalize } from "../../utils/capitalize";
 
 //GraphQuail Mutation
 const EDIT_USER = gql`
@@ -12,32 +13,32 @@ const EDIT_USER = gql`
     $last_name: String
     $email: String
     $city: String
-    $state: String 
-    $gender: String, 
-    $personal_url: String, 
-    $blog_url: String,
-    $twitter_url: String,
-    $linkedin_url: String, 
-    $github_url: String, 
-    $portfolio_url: String,
-    $bio: String,
-    $payment_info: Boolean,
+    $state: String
+    $gender: String
+    $personal_url: String
+    $blog_url: String
+    $twitter_url: String
+    $linkedin_url: String
+    $github_url: String
+    $portfolio_url: String
+    $bio: String
+    $payment_info: Boolean
   ) {
     update(
       first_name: $first_name
       last_name: $last_name
       email: $email
       city: $city
-      state: $state 
-      gender: $gender, 
-      personal_url: $personal_url, 
-      blog_url: $blog_url,
-      twitter_url: $twitter_url,
-      linkedin_url: $linkedin_url, 
-      github_url: $github_url, 
-      portfolio_url: $portfolio_url,
-      bio: $bio,
-      payment_info: $payment_info,
+      state: $state
+      gender: $gender
+      personal_url: $personal_url
+      blog_url: $blog_url
+      twitter_url: $twitter_url
+      linkedin_url: $linkedin_url
+      github_url: $github_url
+      portfolio_url: $portfolio_url
+      bio: $bio
+      payment_info: $payment_info
     ) {
       id
     }
@@ -49,103 +50,168 @@ const DashboardInput = ({ userKey, userValue }) => {
   const [original, setOriginal] = useState(userValue);
   const [editing, setEditing] = useState(false);
   const [user, setUser] = useState({
-    [userKey]: userValue,
+    [userKey]: userValue
   });
 
   const [changeField, changeFieldMutation] = useMutation(EDIT_USER);
   const handleChange = e => {
     setUser({
-      [userKey]: e.target.value,
+      [userKey]: e.target.value
     });
   };
 
   const handleSubmit = e => {
-
     e.preventDefault();
     const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
     //this checks to see if the user pressed accept, but didn't make any changes.
     //if so, no mutation request is made
-    if(original == user[userKey]){
+    if (original == user[userKey]) {
       setEditing(false);
       return;
     }
-    
+
     //check if valid email
-    if(userKey=='email'){
-      if(!user[userKey].match(mailFormat)){
-        console.log('not an email address');
+    if (userKey == "email") {
+      if (!user[userKey].match(mailFormat)) {
+        console.log("not an email address");
         setUser({
-          [userKey]: original,
+          [userKey]: original
         });
         setEditing(false);
         return;
-      } 
+      }
     }
 
     //this makes sure any required fields are not submitted as blank strings
-    if((userKey == "first_name" || userKey == "last_name" || userKey || "email" || userKey == "city" || userKey == "state") && user[userKey] != ""){
-      console.log('submit');
+    if (
+      (userKey == "first_name" ||
+        userKey == "last_name" ||
+        userKey ||
+        "email" ||
+        userKey == "city" ||
+        userKey == "state") &&
+      user[userKey] != ""
+    ) {
+      console.log("submit");
       changeField({ variables: user })
-      .then(res => {
-        setOriginal(user[userKey]);
-        setEditing(false);
-      })
-      .catch(err => {
-        console.log(err);
-      });
+        .then(res => {
+          setOriginal(user[userKey]);
+          setEditing(false);
+        })
+        .catch(err => {
+          console.log(err);
+        });
     } else {
       setUser({
-        [userKey]: original,
+        [userKey]: original
       });
-      setEditing(false)
+      setEditing(false);
       // alert("This is a required field. It cannot be blank.");
-    }    
+    }
 
     console.log(user);
   };
 
   const handleCancel = () => {
     setUser({
-      [userKey]: original,
+      [userKey]: original
     });
     setEditing(false);
   };
 
+  const checkKeyName = () => {
+    if (userKey == "industries") {
+      if (user[userKey][0]) {
+        return user[userKey][0].name;
+      } else {
+        return;
+      }
+    }
+
+    if (userKey == "state") {
+      return user[userKey];
+    }
+
+    return user[userKey];
+  };
+
+  const checkKeyNameForEdit = () => {
+    
+
+if (userKey == "state") {
+      return (
+        <select
+              id="sign-up-state"
+              name="state"
+              placeholder="State"
+              value={user[userKey]}
+              onChange={handleChange}
+              required
+            >
+              <option>Select</option>
+            {statesArray.map(state => (
+                <option value={state} key={state}>
+                  {state}
+                </option>
+              ))}
+          </select>
+      );
+    }
+
+    return (<input
+      name={userKey}
+      placeholder={original}
+      onChange={handleChange}
+      value={user.userKey}
+    />);
+  };
+
   return (
-    <div className='dash-input'>
-      <div className='dash-row'>
-        <span className='dash-heading'>
+    <div className="dash-input">
+      <div className="dash-row">
+        <span className="dash-heading">
           <h2>{capitalize(userKey)}</h2>
         </span>
         <div>
-          
-          {editing ? (
-            <input
-              name={userKey}
-              placeholder={original}
-              onChange={handleChange}
-              value={user.userKey}
-            />
-          ) : 
+          {/* { 
+            editing ? (
+              <input
+                name={userKey}
+                placeholder={original}
+                onChange={handleChange}
+                value={user.userKey}
+              />
+            ) : 
             userKey != 'industries' ? (<p>{user[userKey]}</p>) : user[userKey][0] ? <p>{user[userKey][0].name}</p> : <p></p>}
-          
+          } */}
+          {editing ? 
+          checkKeyNameForEdit()
+            // <input
+            //   name={userKey}
+            //   placeholder={original}
+            //   onChange={handleChange}
+            //   value={user.userKey}
+            // />
+           : (
+            <p>{checkKeyName()}</p>
+          )}
         </div>
       </div>
       <div>
         {editing && (
-          <button onClick={() => handleCancel()} className='cancel-button'>
+          <button onClick={() => handleCancel()} className="cancel-button">
             X
           </button>
         )}
         {editing && (
-          <button onClick={e => handleSubmit(e)} className='accept-button'>
+          <button onClick={e => handleSubmit(e)} className="accept-button">
             &#x2713;
           </button>
         )}
       </div>
       {!editing && (
-        <button className='edit-button' onClick={() => setEditing(true)}>
+        <button className="edit-button" onClick={() => setEditing(true)}>
           &#x1F589;
         </button>
       )}
