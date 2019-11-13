90994877a0b67ba0b9e6032fb5910545488f8d52
jtmccutcheon <jt.mccutcheon@live.com>
2019-11-13 15:37:28 -0500

    wrote test for filling out sign in form and the links on the page

--- src/components/SignInForm/SignInForm.test.js
+++ src/components/SignInForm/SignInForm.test.js
@@ -4,6 +4,8 @@ import * as rtl from "@testing-library/react";
 import ApolloClient from "apollo-boost";
 import SignInForm from "./SignInForm";
 import "@testing-library/jest-dom/extend-expect";
+import { Router, NavLink, BrowserRouter } from "react-router-dom";
+import { createMemoryHistory } from "history";
 
 beforeEach(rtl.cleanup);
 afterEach(rtl.cleanup);
@@ -45,4 +47,40 @@ test("is rendering", () => {
       <SignInForm />
     </ApolloProvider>
   );
+});
+
+test("form and routing", () => {
+  const getToken = () => {
+    let token = localStorage.getItem("token");
+    return token ? `Bearer ${token}` : "";
+  };
+
+  const client = new ApolloClient({
+    uri: "https://quality-hub-gateway-staging.herokuapp.com/",
+    request: operation => {
+      operation.setContext({
+        headers: {
+          Authorization: getToken()
+        }
+      });
+    }
+  });
+  const history = createMemoryHistory({ initialEntries: ["/dashboard"] });
+  rtl.render(
+    <ApolloProvider client={client}>
+      <SignInForm />
+    </ApolloProvider>
+  );
+
+  //fills out the form and clicks Sign in button
+  rtl.fireEvent.change(rtl.getByPlaceholderText(container, "Email"), {
+    target: { value: "dan@quail.com" }
+  });
+  rtl.fireEvent.change(rtl.getByPlaceholderText(container, "Password"), {
+    target: { value: "danquail" }
+  });
+  rtl.fireEvent.click(rtl.getByText(container, "Sign in"));
+  //make sure the router works
+  expect(history.location.pathname).toBe("/dashboard");
+  // Forgot password and Sign up links not working yet will add tests for those when they are active
 });
