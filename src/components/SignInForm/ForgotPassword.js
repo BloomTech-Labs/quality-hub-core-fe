87f4db223a5300891a21b992c1ea41b2b10be007
Nick Gonzalez <gonzaleznick5@gmail.com>
2019-11-14 16:40:16 -0500

    Simple Styling for For Forgot Password form

--- src/components/SignInForm/ForgotPassword.js
+++ src/components/SignInForm/ForgotPassword.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-
+import './SignInForm.scss';
 
 const ForgotPassword = () => {
     const [forgotpass, setForgotPass ] = useState({
@@ -28,6 +28,7 @@ return (
  <h1>Reset Your Passsword</h1>
 
  <h3>Enter your email address and we will send you a link to reset your password!</h3>
+  <div className="forgotpass-Sub">
         <input
             placeholder="Email"
             name="email"
@@ -35,8 +36,9 @@ return (
             onChange={handleChange}
             id="email"
           />
-    <button>Send Request Link</button> 
+    <button className="forgotPass">Send Request Link</button> 
     <Link to="/signIn">Back to sign in</Link>
+    </div>
     </div>
 );
 
