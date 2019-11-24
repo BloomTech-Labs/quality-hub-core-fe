02ac6f8ca9341ae3e529b63e7e1002e2c037886d
Jonathan Ho <johnnyho82@gmail.com>
2019-11-24 14:52:04 -0500

    Remove unneeded code from root App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React, { useState } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import NavBar from './global/components/NavBar';
-import PrivateRoute from './global/components/PrivateRoute';
-// import Footer from './Core/components/Footer';
 import './globalStyles/index.scss';
 import InterviewQ from './global/routes/InterviewQ';
-import Core from './global/routes/Core'
+import Core from './global/routes/Core';
 
 function App() {
 	const [loggedin, setLoggedin] = useState(false);
@@ -19,10 +17,9 @@ function App() {
 				)}
 			/>
 			<div className='not-nav'>
-		<Core loggedin={loggedin} setLoggedin={setLoggedin}/>
-			<InterviewQ loggedin={loggedin} setLoggedin={setLoggedin}/>
+				<Core loggedin={loggedin} setLoggedin={setLoggedin} />
+				<InterviewQ loggedin={loggedin} setLoggedin={setLoggedin} />
 			</div>
-
 		</div>
 	);
 }
