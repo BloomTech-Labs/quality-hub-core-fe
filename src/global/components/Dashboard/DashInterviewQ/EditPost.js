869ccf845079afe3e5bdb89e239f00cf95ed5962
Krishan Nattar <krishan.nattar@gmail.com>
2019-11-27 15:35:40 -0800

    adding edit post mutation

--- src/global/components/Dashboard/DashInterviewQ/EditPost.js
+++ src/global/components/Dashboard/DashInterviewQ/EditPost.js
@@ -3,6 +3,33 @@ import React from 'react';
 import { gql } from 'apollo-boost';
 import { useQuery, useMutation } from '@apollo/react-hooks';
 
+// ID needs to be removed. Backend should pull user ID from token, find current post, then use that to edit post. 
+const EDIT_POST = gql`
+	mutation updatePost(
+		$id: ID! 
+		$price: Int
+		$position: String
+		$industryName: String
+		$description: String
+		$tagString: String
+	) {
+		updatePost(
+			id: $id
+			price: $price
+			position: $position
+			$industryName: $industryName
+			tagString: $tagString
+		) {
+			id
+			price
+			position
+			industryName
+			description
+			tagString
+		}
+	}
+`;
+
 export default function EditPost() {
 	return (
 		<div>
