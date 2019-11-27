2fdd038e10ca22f2c968ad751ead108993474e6b
Jonathan Ho <johnnyho82@gmail.com>
2019-11-27 17:11:41 -0500

    Update InterviewQ LandingPage

--- src/InterviewQ/components/Landing Page/LandingPage.js
+++ src/InterviewQ/components/Landing Page/LandingPage.js
@@ -1,6 +1,10 @@
 // Library
 import React, { useState } from 'react';
+
+// Styles & Icons
 import './LandingPage.scss';
+import Icon from '../../../globalIcons/Icon';
+import { ICONS } from '../../../globalIcons/iconConstants';
 
 // Components
 // import LandingPageCTA from './LandingPageCTA';
@@ -21,9 +25,20 @@ export default function InterviewLandingPage() {
 		<div className='interview-container'>
 			{/* <LandingPageCTA /> */}
 			<div className='interview-landing-page'>
-				<LandingPageHeader />
 				<QNav />
-
+				<div className='interviewq-header-container'>
+					<LandingPageHeader />
+					<div className='interviewq-header-btns'>
+						<button>
+							<Icon icon={ICONS.LIGHTBULB} width={16} height={22} />
+							<span className='becomecoach-btn'>Become a coach</span>
+						</button>
+						<button>
+							<Icon icon={ICONS.FILTER} width={20} height={18} />
+							<span className='filters-btn'>Filters </span>
+						</button>
+					</div>
+				</div>
 				<div className='landingpage-container'>
 					<Search setFields={setFields} fields={fields} />
 					<div className='Coach-List'>
