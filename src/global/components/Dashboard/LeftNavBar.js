7ad80ab30fd755ef8059964f99d83e0b517114a0
Jonathan Ho <johnnyho82@gmail.com>
2019-12-01 15:42:20 -0500

    Update Dashboard Left Nav

--- src/global/components/Dashboard/LeftNavBar.js
+++ src/global/components/Dashboard/LeftNavBar.js
@@ -1,6 +1,10 @@
 // Libraries
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
+
+// Icons
+import Icon from '../../../globalIcons/Icon';
+import { ICONS } from '../../../globalIcons/iconConstants';
 
 // Components
 import DeleteModal from './DeleteModal';
@@ -9,6 +13,8 @@ import DeleteModal from './DeleteModal';
 import useModal from '../../../utils/useModal';
 
 export default function LeftNavBar({ setLoggedin }) {
+	let { pathname } = useLocation();
+	console.log(pathname);
 	const [profileDropdownToggle, setProfileDropdownToggle] = useState(false);
 	const { isShowing, toggle } = useModal();
 
@@ -18,17 +24,15 @@ export default function LeftNavBar({ setLoggedin }) {
 
 	return (
 		<div className='dashboard-left-bar'>
-			<p onClick={() => profileDropdown()}>
-				<span className='gray-square'></span> Profile
-			</p>
+			<div className='dash-left-menu-btn' onClick={() => profileDropdown()}>
+				<Icon icon={ICONS.DASHBOARD} width={24} height={24} color='#3c4043' />
+				<div className='dashnav-txt'>Dashboard</div>
+			</div>
 			{profileDropdownToggle && (
 				<div className='profile-dropdown-links'>
-					<Link to='/dashboard'>Basic Info</Link>
-					<Link to='/dashboard/experience'>Experience</Link>
-					<Link to='/dashboard/paymentinfo'>Payment Info</Link>
-					<Link to='#' onClick={toggle}>
+					<NavLink to='#' onClick={toggle}>
 						Delete Account
-					</Link>
+					</NavLink>
 				</div>
 			)}
 			<DeleteModal
@@ -36,18 +40,50 @@ export default function LeftNavBar({ setLoggedin }) {
 				hide={toggle}
 				setLoggedin={setLoggedin} // GOAL: Have this be a state variable held in Apollo Client cache
 			/>
-
-			<Link to='/dashboard/schedule'>
-				<p>
-					<span className='gray-square'></span>
-					Schedule
-				</p>
-			</Link>
-		
-			<p>
-				<span className='gray-square'></span>{' '}
-				<Link to='/dashboard/interviewq'>InterviewQ</Link>
-			</p>
+			<NavLink activeClassName='dashnavactive' exact to='/dashboard'>
+				<div className='dash-left-menu-btn'>
+					<Icon
+						icon={ICONS.PERSONALINFO}
+						width={18}
+						height={20}
+						color={pathname === '/dashboard' ? '#096dd9' : '#3c4043'}
+					/>
+					<div className='dashnav-txt'>Personal Info</div>
+				</div>
+			</NavLink>
+			<NavLink activeClassName='dashnavactive' to='/dashboard/coachinfo'>
+				<div className='dash-left-menu-btn'>
+					<Icon
+						icon={ICONS.COACHINFO}
+						width={24}
+						height={24}
+						color={pathname === '/dashboard/coachinfo' ? '#096dd9' : '#3c4043'}
+					/>
+					<div className='dashnav-txt'>Coach Info</div>
+				</div>
+			</NavLink>
+			<NavLink activeClassName='dashnavactive' to='/dashboard/schedule'>
+				<div className='dash-left-menu-btn'>
+					<Icon
+						icon={ICONS.SCHEDULE}
+						width={24}
+						height={24}
+						color={pathname === '/dashboard/schedule' ? '#096dd9' : '#3c4043'}
+					/>
+					<div className='dashnav-txt'> Schedule</div>
+				</div>
+			</NavLink>
+			<NavLink activeClassName='dashnavactive' to='/dashboard/setting'>
+				<div className='dash-left-menu-btn'>
+					<Icon
+						icon={ICONS.SETTING}
+						width={24}
+						height={24}
+						color={pathname === '/dashboard/setting' ? '#096dd9' : '#3c4043'}
+					/>
+					<div className='dashnav-txt'>Setting</div>
+				</div>
+			</NavLink>
 		</div>
 	);
 }
--- src/global/components/Dashboard/PersonalInfo.js
+++ src/global/components/Dashboard/PersonalInfo.js
@@ -9,7 +9,12 @@ export default function PersonalInfo() {
 	return (
 		<div className='personalinfo-header'>
 			<div className='circle-blue'>
-				<Icon icon={ICONS.PERSONALINFO} width={26} height={28} color='white' />
+				<Icon
+					icon={ICONS.PERSONALINFOBIG}
+					width={26}
+					height={28}
+					color='white'
+				/>
 			</div>
 			<h1>Personal Info</h1>
 		</div>
