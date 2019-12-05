26bf24034acafa53c13b1ace31887cc5fcc662e5
Jonathan Ho <johnnyho82@gmail.com>
2019-12-04 20:53:49 -0500

    Clean up code to reduce errors

--- src/InterviewQ/components/CoachList/CoachCardModal.js
+++ src/InterviewQ/components/CoachList/CoachCardModal.js
@@ -1,37 +1,33 @@
-import React , { useState, useEffect } from 'react';
-import CoachCard from "./CoachCardExpand.js";
-
+import React, { useState, useEffect } from 'react';
+import CoachCard from './CoachCardExpand.js';
 
 //Styles
 import './CoachCardModal.scss';
 // import Icon from '../../../globalIcons/Icon';
 // import { ICONS } from '../../../globalIcons/iconConstants';
 
-const CoachCardModal = ({post}) => {
+const CoachCardModal = ({ post }) => {
 	const [open, setOpen] = useState(false);
-    
 
-    useEffect(() => {
+	useEffect(() => {
 		if (open) {
-			document.getElementById('overlay-coachcard-expand').style.display = 'block';
+			document.getElementById('overlay-coachcard-expand').style.display =
+				'block';
 		} else {
-			document.getElementById('overlay-coachcard-expand').style.display = 'none';
+			document.getElementById('overlay-coachcard-expand').style.display =
+				'none';
 		}
 	}, [open]);
 
-
-    return (
-        <div className="coach-card-modal-text">
-            <div id="overlay-coachcard-expand"></div>
-            <button className="coach-card-modal-text" onClick={() => setOpen(!open)}> 
-              <p className='coach-card-modal-text coachcard-seemore'>See more </p>
-            </button>
-            {open && (
-                  <CoachCard setOpen={setOpen} post={post}/>
-            )}
-        </div>
-    )
+	return (
+		<div className='coach-card-modal-text'>
+			<div id='overlay-coachcard-expand'></div>
+			<button className='coach-card-modal-text' onClick={() => setOpen(!open)}>
+				<p className='coach-card-modal-text coachcard-seemore'>See more </p>
+			</button>
+			{open && <CoachCard setOpen={setOpen} post={post} />}
+		</div>
+	);
 };
-
 
 export default CoachCardModal;
