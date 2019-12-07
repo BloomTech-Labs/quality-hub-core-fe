24c615fbd9f17dab8fadb0f7ee26272d21efa3b7
Krishan Nattar <krishan.nattar@gmail.com>
2019-12-06 16:18:09 -0800

    more stylings for grid dropdown

--- src/global/components/NavBar/GridDropdown.js
+++ src/global/components/NavBar/GridDropdown.js
@@ -8,12 +8,12 @@ import favicon from '../../../globalIcons/favicon.svg';
 import Icon from '../../../globalIcons/Icon';
 import { ICONS } from '../../../globalIcons/iconConstants';
 
-import {Interviewqicon} from '../../../globalIcons/interviewqicon';
-import {Resumeq} from '../../../globalIcons/resumeqicon';
-import {Designqicon} from '../../../globalIcons/designqicon';
-import {Codeqicon} from '../../../globalIcons/codeqicon';
-import {Recruiterqicon} from '../../../globalIcons/recruiterqicon';
-import {Networkqicon} from '../../../globalIcons/networkqicon';
+import { Interviewqicon } from '../../../globalIcons/interviewqicon';
+import { Resumeq } from '../../../globalIcons/resumeqicon';
+import { Designqicon } from '../../../globalIcons/designqicon';
+import { Codeqicon } from '../../../globalIcons/codeqicon';
+import { Recruiterqicon } from '../../../globalIcons/recruiterqicon';
+import { Networkqicon } from '../../../globalIcons/networkqicon';
 
 const GridDropdown = props => {
 	const node = useRef();
@@ -36,47 +36,55 @@ const GridDropdown = props => {
 
 	return (
 		<div ref={node}>
-			<div className='grid-menu grid-icon' onClick={() => setOpen(!open)}>
+			<div className="grid-menu grid-icon" onClick={() => setOpen(!open)}>
 				<Icon icon={ICONS.GRID} width={24} height={24} />
 			</div>
 
 			{open && (
-				<div className='dropdown-grid-content dropdown-icons'>
-					<div className='test-css-grid'>
-						<Link to='/interviewq' className='box' onClick={() => setOpen(false)}>
-		
+				<div className="dropdown-grid-content dropdown-icons">
+					{/* <div className="test-css-grid"> */}
+					<div className="grid-dropdown-top-content">
+					<div className="grid-dropdown-top-row-1">
+						<Link
+							to="/interviewq"
+							className="box"
+							onClick={() => setOpen(false)}>
 							{Interviewqicon()}
 							<p>InterviewQ</p>
 						</Link>
 
-						<Link to='#' className='box' onClick={() => setOpen(false)}>
+						<Link to="#" className="box" onClick={() => setOpen(false)}>
 							{Resumeq()}
 							<p>ResumeQ</p>
 						</Link>
 
-						<Link to='#' className='box' onClick={() => setOpen(false)}>
-							 {Designqicon()}
+						<Link to="#" className="box" onClick={() => setOpen(false)}>
+							{Designqicon()}
 							<p>DesignQ</p>
 						</Link>
+						</div>
+						<div className="grid-dropdown-top-row-1">
 
-						<Link to='#' className='box' onClick={() => setOpen(false)}>
+						<Link to="#" className="box" onClick={() => setOpen(false)}>
 							{Codeqicon()}
 							<p>CodeQ</p>
 						</Link>
 
-						<Link to='#' className='box' onClick={() => setOpen(false)}>
+						<Link to="#" className="box" onClick={() => setOpen(false)}>
 							{Networkqicon()}
 							<p>NetworkQ</p>
 						</Link>
 
-						<Link to='#' className='box' onClick={() => setOpen(false)}>
+						<Link to="#" className="box" onClick={() => setOpen(false)}>
 							{Recruiterqicon()}
 							<p>Recruiterqicon</p>
 						</Link>
-					</div>
+						</div>
+						</div>
+					{/* </div> */}
+						<hr className="grid-dropdown-bottom-hr" />
 					<div className="grid-dropdown-bottom-content">
-					<hr className="grid-dropdown-bottom-hr" />
-					<p className="grid-dropdown-more">More</p>
+						<p className="grid-dropdown-more">More</p>
 					</div>
 				</div>
 			)}
