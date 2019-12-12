279e8aed29a696c2044a8908d942fc4e83074ff8
Jordan <47953030+jordandoan@users.noreply.github.com>
2019-12-12 09:47:22 -0800

    Revert "DAMN"

---
+++ src/InterviewQ/components/DashInterviewQ/Video.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Video() {
+	return (
+		<div>
+			<h2>InterviewQ Video Conferencing</h2>
+			<p>
+				This component will allow Coach and Seeker to conduct their practice
+				interview using WebRTC technology
+			</p>
+		</div>
+	);
+}
