abd0619787ea701518e6326f7c9b4bb362a125f9
Jordan <jordandoan@hotmail.com>
2019-12-13 11:56:58 -0800

    removed commented code in modal

--- src/InterviewQ/components/LandingPage/CoachList/subs/2_CoachCardModal/CoachCardModal.js
+++ src/InterviewQ/components/LandingPage/CoachList/subs/2_CoachCardModal/CoachCardModal.js
@@ -3,15 +3,12 @@ import CoachCard from './CoachCardExpand';
 
 //Styles
 import '../../CoachCardModal.scss';
-// import Icon from '../../../globalIcons/Icon';
-// import { ICONS } from '../../../globalIcons/iconConstants';
 
 const CoachCardModal = ({ post }) => {
 	const [open, setOpen] = useState(false);
 
 	return (
 		<div className='coach-card-modal-text'>
-			{/* <div id='overlay-coachcard-expand' onClick={() => setOpen(!open)}></div> */}
 			<button className='coach-card-modal-text' onClick={() => setOpen(!open)}>
 				<p className='coach-card-modal-text coachcard-seemore'>See more </p>
 			</button>
