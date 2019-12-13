93189bb754b00a4af4cfe0d03eeb5c750218fd89
Jordan <jordandoan@hotmail.com>
2019-12-13 15:01:18 -0800

    Fixed links in coach cards

--- src/InterviewQ/components/LandingPage/CoachList/subs/1_CoachCard/CoachCard.js
+++ src/InterviewQ/components/LandingPage/CoachList/subs/1_CoachCard/CoachCard.js
@@ -13,7 +13,8 @@ import CoachModal from '../2_CoachCardModal/CoachCardModal';
 const CoachCard = ({ post }) => {
 	let { coach } = post;
 	let maxWidth = 100;
-
+  const linkedin = coach.linkedin_url && (coach.linkedin_url.startsWith('http') ? coach.linkedin : `http://${coach.linkedin_url}`)
+  const twitter = coach.twitter_url && (coach.twitter_url.startsWith('http') ? coach.linkedin: `http://${coach.twitter_url}`)
 	return (
 		<div className='coach-card'>
 			<div className='coachcard-header'>
@@ -28,10 +29,6 @@ const CoachCard = ({ post }) => {
 						<img src={coach.image_url} alt='Coach Profile Pic' />
 					) : (
             <Icon icon={ICONS.BLANK_AVATAR} />
-						// <img
-						// 	src='https://www.birdorable.com/img/bird/th440/california-quail.png'
-						// 	alt='Coach Profile Pic'
-						// />
 					)}
 				</div>
 			</div>
@@ -75,7 +72,7 @@ const CoachCard = ({ post }) => {
 				<div className='coachcard-links'>
 					{post.coach.linkedin_url && (
 						<a
-							href={post.coach.linkedin_url}
+							href={linkedin}
 							target='_blank'
 							rel='noopener noreferrer'>
 							<Icon icon={ICONS.LINKEDIN} width={24} height={24} />
@@ -83,7 +80,7 @@ const CoachCard = ({ post }) => {
 					)}
 					{post.coach.twitter_url && (
 						<a
-							href={post.coach.twitter_url}
+							href={twitter}
 							target='_blank'
 							rel='noopener noreferrer'>
 							<Icon icon={ICONS.TWITTER} width={24} height={24} />
