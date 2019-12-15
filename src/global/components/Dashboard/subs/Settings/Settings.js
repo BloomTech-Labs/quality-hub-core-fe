e3ab479b1dfe6b0451cdfda257246c881c9fa6c6
Jonathan Ho <johnnyho82@gmail.com>
2019-12-15 01:13:21 -0500

    Commit before merge

--- src/global/components/Dashboard/subs/Settings/Settings.js
+++ src/global/components/Dashboard/subs/Settings/Settings.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import './settings.scss';
+
 import SettingsHeader from './subs/1_SettingsHeader';
 import DeleteAccount from './subs/2_DeleteAccount';
 
 export default function Setting() {
 	return (
-		<div>
+		<div className='settings-wrapper'>
 			<SettingsHeader />
 			<DeleteAccount />
 		</div>
