ebffc38a8387a204d7cc99510cf1046a43b9e548
Jonathan Ho <johnnyho82@gmail.com>
2019-12-17 16:28:16 -0500

    Commit before pull

---
+++ src/InterviewQ/components/DashInterviewQ/subs/DeleteCoachPost/DeleteCoachPost.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import './DeleteCoachPost.scss';
+
+import DeletePostModal from './DeletePostModal/00_DeletePostModal';
+
+import useModal from '../../../../../global/utils/useModal';
+
+export default function DeletePost() {
+	const { isShowing, toggle } = useModal();
+
+	return (
+		<div>
+			<div className='delete-coach-post'>
+				<h2>Delete Coach Post</h2>
+				<div className='delete-post'>
+					<span className='delete-warning'>
+						<p>If you delete your post, you can't reverse this action</p>
+					</span>
+					<button className='delete-post-btn' onClick={toggle}>
+						<p>Delete Post</p>
+					</button>
+				</div>
+			</div>
+			<DeletePostModal isShowing={isShowing} hide={toggle} />
+		</div>
+	);
+}
