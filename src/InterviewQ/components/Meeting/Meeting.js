bb422cd02c662dcb7b6e1cc130f268678b2526bf
Ryan Ziegenfus <thewebcoderz@gmail.com>
2019-12-18 01:52:53 -0500

    remove cancel appointment button from past appointments

--- src/InterviewQ/components/Meeting/Meeting.js
+++ src/InterviewQ/components/Meeting/Meeting.js
@@ -1,7 +1,15 @@
 // Library
 import React, { useState, useEffect } from 'react';
+import { Route } from 'react-router-dom';
+import Waiting from './subs/Waiting'
+import Room from './subs/Room'
 
 export default function Meeting() {
 	
-	return 
+	return (
+        <>
+            <Route path='/interviewq/meeting' component={Waiting} />
+            <Route path='/interviewq/meeting/room' component={Room} />
+        </>
+    )
 }
