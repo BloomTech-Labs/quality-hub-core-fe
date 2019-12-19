77822a1efccb6a9134f4ac0fb0e8175c2663aed5
Jonathan Ho <johnnyho82@gmail.com>
2019-12-19 14:15:55 -0500

    Commit before pull

--- src/InterviewQ/components/History/subs/CoachHistory/4_CoachHistoryReview.js
+++ src/InterviewQ/components/History/subs/CoachHistory/4_CoachHistoryReview.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import HistoryReview from '../HistoryReview';
-import CoachResponse from './5_CoachResponse';
+import CoachResponse from '../CoachResponse/CoachResponse';
 
 export default function CoachHistoryReview({ booking }) {
+	const [showResponse, setShowResponse] = useState(false);
+
 	return (
 		<div className='coach-history-review'>
 			{booking.review ? (
-				<div>
+				<>
 					<HistoryReview booking={booking} />
-					<CoachResponse />
-				</div>
+					<button
+						className='toggle-coach-response-btn'
+						onClick={() => setShowResponse(!showResponse)}>
+						Reply to this review
+					</button>
+					{showResponse && <CoachResponse />}
+				</>
 			) : (
 				<p>{booking.seeker.first_name} has not yet submitted a review.</p>
 			)}
---
+++ src/InterviewQ/components/History/subs/CoachResponse/AddCoachResponse.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function AddCoachResponse() {
+	return <div>Add Coach Response</div>;
+}
