0c8dded8b033b3848eb24de60b7b87b3fa0402f4
Jonathan Ho <johnnyho82@gmail.com>
2019-12-30 02:28:19 -0500

    Reorganize Customer and Payment components

--- src/global/components/Dashboard/subs/PersonalInfo/subs/PaymentInfo/PaymentInfo.js
+++ src/global/components/Dashboard/subs/PersonalInfo/subs/PaymentInfo/PaymentInfo.js
@@ -1,9 +1,10 @@
 // Libraries
 import React from 'react';
-import { gql } from 'apollo-boost';
-import { useMutation } from '@apollo/react-hooks';
 
 import './Payment.scss';
+
+import Coach from './Coach';
+import Customer from './Customer';
 
 export default function PaymentInfo() {
 	return (
@@ -11,18 +12,8 @@ export default function PaymentInfo() {
 			<div className='dash-payment-header'>
 				<h2>Payments</h2>
 			</div>
-			<div className='dash-customer-connectstripe'>
-				<h3>Set up Customer Account on Stripe</h3>
-				<div className='dash-payment-row'>
-					<p>
-						Click above to set up your Stripe account to pay for QualityHub
-						services
-					</p>
-					<button>
-						<a href='#'>Connect to Stripe</a>
-					</button>
-				</div>
-			</div>
+			<Coach />
+			<Customer />
 		</div>
 	);
 }
