9efbb5ec6b6b79cb01e2cb5d7d58263ec1a8a70d
Aaron Clark <aaronpclark92@gmail.com>
2019-12-30 12:29:39 -0600

    added import for react-router-dom in rq route, corrected marketplace import error

---
+++ src/ResumeQ/components/Marketplace/ReviewerList/1_ReviewerCard/ReviewerCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const ReviewerCard = () => {
+
+
+  return (
+    <h2>Reviewer Card Presentational Component</h2>
+  )
+}
+
+export default ReviewerCard;
--- src/global/routes/ResumeQ.js
+++ src/global/routes/ResumeQ.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
 import ResumeQContainer from '../../ResumeQ/ResumeQ';
 import { Resumeq } from '../icons/resumeqicon';
 
 const ResumeQ = () => {
-  return
-  <Switch>
-    <Route path='/resumeq' component={ResumeQContainer} />
-  </Switch>
+  return (
+    <Switch>
+      <Route path='/resumeq' component={ResumeQContainer} />
+    </Switch>)
 }
 
 export default ResumeQ;
