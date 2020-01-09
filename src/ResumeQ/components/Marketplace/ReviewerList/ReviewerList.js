908baa8ec728896faf5b7b0e8626f2e906ce4f6b
Aaron Clark <aaronpclark92@gmail.com>
2020-01-09 12:47:30 -0600

    refactor GET_REVIEWER_LISTINGS in ReviewerList

--- src/ResumeQ/components/Marketplace/ReviewerList/ReviewerList.js
+++ src/ResumeQ/components/Marketplace/ReviewerList/ReviewerList.js
@@ -1,28 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { useQuery } from '@apollo/react-hooks'
 
 import ReviewerCard from './subs/1_ReviewerCard'
-import Search from './subs/0_Search'
+// import Search from './subs/0_Search'
 
 import './ReviewerList.scss'
 
 import {
   GET_REVIEWER_LISTINGS,
-  RESUME_Q
+
 } from './Resolvers'
 
 // this is a stateful component which will hold the list of reviewers and render each to a card component
 
 const ReviewerList = () => {
-  const listings = useQuery(GET_REVIEWER_LISTINGS);
+  const [fields, setFields] = useState({
+    price: '',
+    orderBy: "id_ASC"
+  })
+
+
+  const { refect, loading, data } = useQuery(GET_REVIEWER_LISTINGS, {
+    fetchPolicy: 'network-only'
+  });
+
+  console.log(`REVIEWER_LISTINGS query data`, data)
+
+  // const listings = useQuery(GET_REVIEWER_LISTINGS);
   return (
     <>
       <div>
         {/* add routes for components as they're built out */}
         <h1>ReviewerList Component</h1>
-        {listings.data && (
-          listings.data.reviewerListings.map(listing => {
+        {data && (
+          data.reviewerListings.map(listing => {
             return <ReviewerCard key={listing.id} listing={listing} />
           }))}
       </div>
@@ -30,26 +42,6 @@ const ReviewerList = () => {
   )
 }
 
-<div className='coach-list-container'>
-  {/* <div className={toggleFilter ? '' : 'hidden'}> */}
-  <Search
-    setFields={setFields}
-    fields={fields}
-    refetch={refetch}
-    toggleFilter={toggleFilter}
-    setToggleFilter={setToggleFilter}
-  />
-  {/* </div> */}
-  {loading && <Loading />}
-  {!loading && data && (
-    <div className='coach-list'>
-      {console.log(data.posts[0])}
-      {data.posts.map(post => (
-        <CoachCard key={post.id} post={post} history={history} />
-      ))}
-    </div>
-  )}
-</div>
-	);
+
 
 export default ReviewerList;
