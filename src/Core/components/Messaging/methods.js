32d8556e82301db69b8e5aa76286409e7464fc20
Julie <julieantonio38@gmail.com>
2020-01-10 21:15:08 -0500

    imporoved chat

--- src/Core/components/Messaging/methods.js
+++ src/Core/components/Messaging/methods.js
@@ -1,49 +1,5 @@
-import Chatkit from '@pusher/chatkit-client';
-import { gql } from 'apollo-boost';
-import { GET_QH_USER, GET_CHAT_IMG } from './resolvers';
 import { ChatManager, TokenProvider } from '@pusher/chatkit-client';
 import { tokenUrl, instanceLocator } from './config';
-import { useQuery } from '@apollo/react-hooks';
-
-// function createPrivateRoom(id) {
-//   const { currentUser, rooms } = this.state;
-//   const roomName = `${currentUser.id}_${id}`;
-
-//   const isPrivateChatCreated = rooms.filter(room => {
-//     if (room.customData && room.customData.isDirectMessage) {
-//       const arr = [currentUser.id, id];
-//       const { userIds } = room.customData;
-
-//       if (arr.sort().join('') === userIds.sort().join('')) {
-//         return {
-//           room,
-//         };
-//       }
-//     }
-
-//     return false;
-//   });
-
-//   if (isPrivateChatCreated.length > 0) {
-//     return Promise.resolve(isPrivateChatCreated[0]);
-//   }
-
-//   return currentUser.createRoom({
-//     name: `${roomName}`,
-//     private: true,
-//     addUserIds: [`${id}`],
-//     customData: {
-//       isDirectMessage: true,
-//       userIds: [currentUser.id, id],
-//     },
-//   });
-// }
-
-// export function sendDM(id) {
-//   createPrivateRoom.call(this, id).then(room => {
-//     connectToRoom.call(this, room.id);
-//   });
-// }
 
 const chatManager = new ChatManager({
   instanceLocator,
@@ -55,8 +11,7 @@ const chatManager = new ChatManager({
 
 export  const startDM = (recipient, recipientId, sender, history) => {
   const userId = localStorage.getItem('id')
-  
-  // const sender = await `${data.me.first_name} ${data.me.last_name}`
+
   chatManager.connect()
   .then(currentUser => {
     currentUser.createRoom({
@@ -65,7 +20,6 @@ export  const startDM = (recipient, recipientId, sender, history) => {
     private: true,
     addUserIds: [userId, recipientId],
   }).then(room => {
-    // history.push('/interviewq/inbox')
     	history.push({
 				pathname: '/interviewq/inbox',
 				state: {
@@ -85,73 +39,115 @@ export  const startDM = (recipient, recipientId, sender, history) => {
 })
 }
 
-export const getRooms = (setConvList) => {
-  chatManager.connect({
-    onAddedToRoom: room => {
-    }
-}).then(currentUser => {
-  setConvList(currentUser.rooms.map(channel => {
-    const splitName= channel.name.split('-');
-  if (channel.createdByUserId === localStorage.getItem('id')){
-    return { name: channel.name, id: channel.id, createdByUserId: channel.createdByUserId, displayName: splitName[1], unreadCount: channel.unreadCount }
-  }  
-    return { name: channel.name, id: channel.id, createdByUserId: channel.createdByUserId, displayName: splitName[0], unreadCount: channel.unreadCount }
-  }))
-})
-}
-// export const getRooms =  (setConvList, data) => {
-
-//   chatManager.connect({
-//     onAddedToRoom: room => {
-//     }
-// }).then(currentUser => {
-//   setConvList(currentUser.rooms.map(async (channel) => {
-//     const splitName= channel.name.split('-');
-//     await data
-//   if (channel.createdByUserId === localStorage.getItem('id')){
-//     return { name: channel.name, id: channel.id, createdByUserId: channel.createdByUserId, displayName: splitName[1] }
-//   }  
-//     return { name: channel.name, id: channel.id, createdByUserId: channel.createdByUserId, displayName: splitName[0] }
-//   }))
-// })
-// }
-
-export const connectToRoom = (roomId, chatLog, setChatLog) => {
-  //window.location.reload();
+export const checkRooms = (setRooms) => {
   chatManager.connect()
   .then(currentUser => {
     // console.log(currentUser)
-    let messageObj;
-    let messageArray = [];
-    currentUser.subscribeToRoom({
-      roomId: roomId,
-      //roomId: roomId,
-      // messageLimit: 0,
-     hooks: {
-         onMessage: message => {
-          const  messageElements =  document.getElementById('message-list-div').childNodes;
-           messageObj = { text: message.text, senderId: message.senderId };
-           messageArray.push(messageObj);
+    setRooms(currentUser.rooms)
+  })
+}
+
+export const getRooms = (setConvList, setTheCurrentUser, setToggle, toggle, roomList, setRoomList) => {
+  // console.log("GET ROOMSSS!!!@ AHHGHGH!!!!")
+  let list;
+  // console.log(setRoomList)
+  // console.log(roomList)
+  chatManager.connect({
+    // onAddedToRoom: room => {
+      onRoomUpdated: room =>{
+        console.log(room)
+        // console.log(roomList)
+        // if(setToggle){
+          if(list){
+            let changed = true;
+            for(let i =0; i< list.length; i++){
+              if(list[i].id===room.id){
+                console.log(room.id);
+                list[i] = room;
+                changed = false;
+                break;
+              }
+            }
+            if(changed==true){
+              list.push(room);
+            }
+
+
+            // console.log(room)
+            setConvList(list.map(channel => {
+              const splitName= channel.name.split('-');
+            if (channel.createdByUserId === localStorage.getItem('id')){
+              return { name: channel.name, id: channel.id, createdByUserId: channel.createdByUserId, displayName: splitName[1], unreadCount: channel.unreadCount, lastMessageAt: channel.lastMessageAt }
+            }  
+              return { name: channel.name, id: channel.id, createdByUserId: channel.createdByUserId, displayName: splitName[0], unreadCount: channel.unreadCount, lastMessageAt: channel.lastMessageAt }
+            }))
+          }
+          // console.log("UPDATED ROOM")
+          // setToggle(toggle+1);
+        // }
+      }
+      // },
+    }).then(currentUser => {
+      
+
+  // setTheCurrentUser(currentUser);
+// setRoomList(currentUser.rooms);
+list = currentUser.rooms
+  setConvList(currentUser.rooms.map(channel => {
+    const splitName= channel.name.split('-');
+  if (channel.createdByUserId === localStorage.getItem('id')){
+    return { name: channel.name, id: channel.id, createdByUserId: channel.createdByUserId, displayName: splitName[1], unreadCount: channel.unreadCount, lastMessageAt: channel.lastMessageAt }
+  }  
+    return { name: channel.name, id: channel.id, createdByUserId: channel.createdByUserId, displayName: splitName[0], unreadCount: channel.unreadCount, lastMessageAt: channel.lastMessageAt }
+  }))
+})
+}
+
+export const connectToRoom = (roomId, chatLog, setChatLog, setTheCurrentUser) => {
+  chatManager.connect()
+  .then(currentUser => {
+
+      let messageObj;
+      let messageArray = [];
+      
+      currentUser.subscribeToRoom({
+        roomId: roomId,
+        hooks: {
+          onMessage: message => {
+            const messageElements = document.getElementById('message-list-div')
+						.childNodes;
+            messageObj = { text: message.text, senderId: message.senderId };
+            messageArray.push(messageObj);
             setChatLog(messageArray);
-           const messageDiv = document.createElement('li');
-          //  messageDiv.textContent = `${message.sender.name}: ${message.text}`
-          messageDiv.textContent = `${message.text}`
-           messageDiv.id = message.id
-        let check = false;
-           for(let x=0; x < messageElements.length; x++){
-              if(messageElements[x].id == message.id){
-                check = true;
-              }
-            }
-            if(!check){
-              document.querySelector('.chat-messages').appendChild(messageDiv);
-              if (message.senderId === localStorage.getItem('id')){
-                messageDiv.classList.add('sentMessage')
-              }
-            }
-           return
-         },
-       },
-     });
+            setTheCurrentUser(currentUser);
+					const messageDiv = document.createElement('li');
+          messageDiv.textContent = `${message.text}`;
+          
+					messageDiv.id = message.id;
+					let check = false;
+					for (let x = 0; x < messageElements.length; x++) {
+						if (messageElements[x].id == message.id) {
+							check = true;
+						}
+					}
+					if (!check) {
+						document.querySelector('.chat-messages').appendChild(messageDiv);
+						if (message.senderId === localStorage.getItem('id')) {
+							messageDiv.classList.add('sentMessage');
+						}
+					}
+          // return;
+          currentUser.setReadCursor({
+
+            roomId: roomId,
+
+            position: message.id,
+
+          });
+          //return;
+				},
+			},
+    });
+    
    })
  } 
