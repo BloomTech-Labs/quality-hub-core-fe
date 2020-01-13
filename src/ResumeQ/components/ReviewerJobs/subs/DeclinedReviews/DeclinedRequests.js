0df060ff030bc0bdf30f7e75f8ff14340df713da
Aaron Clark <aaronpclark92@gmail.com>
2020-01-13 17:37:03 -0600

    moved queries to subcomponents, built presentational component to render each type of ResumeReview entry

--- src/ResumeQ/components/ReviewerJobs/subs/DeclinedReviews/DeclinedRequests.js
+++ src/ResumeQ/components/ReviewerJobs/subs/DeclinedReviews/DeclinedRequests.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import { useQuery } from '@apollo/react-hooks'
 
 import ResumeReviewEntry from '../ResumeReviewEntry'
+import { DECLINED_RESUME_REVIEWS } from '../../Resolvers'
 
 
 const DeclinedRequests = () => {
+
+    const { refetch, loading, data } = useQuery(DECLINED_RESUME_REVIEWS, {
+        fetchPolicy: 'network-only'
+    })
+
+    console.log(`DeclinedRequests / data`, data)
+
+    const declinedArray = !loading && data.declinedResumeReviews.map(review => {
+        return {
+            ...review,
+            status: "Declined"
+        }
+    })
+
+
     return (
         <div>
 
--- src/ResumeQ/components/ReviewerJobs/subs/ResumeReviewEntry/ResumeReviewEntry.js
+++ src/ResumeQ/components/ReviewerJobs/subs/ResumeReviewEntry/ResumeReviewEntry.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from 'react';
 
-const ResumeReviewEntry = (props) => {
+const ResumeReviewEntry = ({
+  request,
+  request: { coach, seeker, createdAt }
+}) => {
+
   return (
     <div>
-
+      <div>
+        <p>{createdAt}</p>
+        <p>{seeker.first_name} {seeker.last_name}</p>
+        <p>{seeker.email}</p>
+        <button>Accept</button>
+        <button>Decline</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ResumeReviewEntry
+export default ResumeReviewEntry;
