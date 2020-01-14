8b0f2b71f0f342958c88bb5c84e3d538aff4d814
Taran <thetaranneeld@gmail.com>
2020-01-14 16:02:17 -0700

    added tabs and completed ReviewsHistory component

--- src/ResumeQ/components/ReviewerJobs/ReviewerJobs.js
+++ src/ResumeQ/components/ReviewerJobs/ReviewerJobs.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import { useQuery } from '@apollo/react-hooks'
 
+import './LeftNav.scss'
 
 import RequestedReview from './subs/RequestedReviews'
 import AcceptedReviews from './subs/AcceptedReviews'
@@ -12,22 +13,36 @@ const ReviewerJobs = () => {
 
     // This component holds components for each type of ResumeReview entry. Each entry contains its own state and queries
 
+    function openTab(tab) {
+        let tabs = document.getElementsByClassName('requestTab');
+        for (let i = 0; i < tabs.length; i++) {
+        tabs[i].style.display = 'none';
+        }
+        document.getElementById(tab).style.display = 'block';
+      }
+
     return (
         <div>
-            <div className='requestTab'>
+            <div className='QNav-row'>
+                <button onClick={() => openTab('requests')} className='QNav-btn'>Pending</button>
+                <button onClick={() => openTab('accepted')} className='QNav-btn'>Accepted</button>
+                <button onClick={() => openTab('declined')} className='QNav-btn'>Declined</button>
+                <button onClick={() => openTab('history')} className='QNav-btn'>History</button>
+            </div>
+            <div id='requests' className='requestTab'>
                 <h1>Requested Reviews</h1>
                 <RequestedReview />
             </div>
-            <div className='requestTab' >
+            <div id='accepted' className='requestTab' style={{display: 'none'}}>
                 <h1>Reviews in Progress</h1>
                 <AcceptedReviews />
             </div>
-            <div className='requestTab' >
+            <div id='declined' className='requestTab' style={{display: 'none'}}>
                 <h1>Declined Reviews</h1>
                 <DeclinedReviews />
             </div>
-            <div className='requestTab' >
-                <h1>Past Reviews</h1>
+            <div id='history' className='requestTab' style={{display: 'none'}}>
+                <h1>Completed Reviews</h1>
                 <ReviewsHistory />
             </div>
         </div>
