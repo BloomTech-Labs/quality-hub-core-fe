1644f1e36a2d525ba3c986c2f58752619130a65e
Hugo Oliveira <oliveira.hugo.c@gmail.com>
2020-01-26 03:02:59 -0600

    Changed loading icon. Included dark mode gif for future use

--- src/global/components/Loading/Loading.js
+++ src/global/components/Loading/Loading.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React from "react";
+import LoadingGif from "./spinner.gif";
+import LoadingGifDark from "./spinnerdark.gif";
 
 const Loading = props => {
-	return (
-		<div>
-			{/* Copied loader off of random site. Would be cool to have our own custom one?! */}
-			<img
-				src='loading2.gif'
-				height='100px'
-				width='100px'
-				alt='loading animation'
-			/>
-		</div>
-	);
+  return (
+    <div>
+      {/* Copied loader off of random site. Would be cool to have our own custom one?! */}
+      <img
+        src={LoadingGif}
+        height="100px"
+        width="100px"
+        alt="loading animation"
+      />
+    </div>
+  );
 };
 
 export default Loading;
