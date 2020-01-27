9f9d8d2d7fa73eecdd251921a17a6ddbc518828d
Zac Higgins <zachiggins@Higgins-iMac.local>
2020-01-27 16:34:28 -0700

    adjusted existing media query breakpoints and added additional breakpoints for better laytout responsiveness.

--- src/InterviewQ/components/LandingPage/CoachList/subs/0_Search/Search.js
+++ src/InterviewQ/components/LandingPage/CoachList/subs/0_Search/Search.js
@@ -78,86 +78,92 @@ export default function Search({ fields, setFields, refetch }) {
 		<div className='search-container'>
 			<h2>Search Coach</h2>
 			<div className='search-dropdowns'>
-				<div className='search-field'>
-					{/* <label>Industry</label> */}
-					<select
-						name='industry'
-						placeholder='Industry'
-						onChange={handleChange}
-						value={fields.industry}
-						required>
-						<option value='' disabled hidden>
-							Industry
+				<div className='search-dropdowns'>
+					<div className='search-field'>
+						{/* <label>Industry</label> */}
+						<select
+							name='industry'
+							placeholder='Industry'
+							onChange={handleChange}
+							value={fields.industry}
+							required>
+							<option value='' disabled hidden>
+								Industry
 						</option>
-						<option value=''>All</option>
-						{ind_data &&
-							ind_data.industries.map(({ name }) => (
-								<option key={name} value={name}>
-									{name}
-								</option>
-							))}
-					</select>
-				</div>
-				<div className='search-field'>
-					{/* <label>Price</label> */}
-					<select
-						name='price'
-						placeholder='Price'
-						onChange={handleChange}
-						value={fields.price}
-						required>
-						<option value='' disabled hidden>
-							Price
+							<option value=''>All</option>
+							{ind_data &&
+								ind_data.industries.map(({ name }) => (
+									<option key={name} value={name}>
+										{name}
+									</option>
+								))}
+						</select>
+					</div>
+					<div className='search-field'>
+						{/* <label>Price</label> */}
+						<select
+							name='price'
+							placeholder='Price'
+							onChange={handleChange}
+							value={fields.price}
+							required>
+							<option value='' disabled hidden>
+								Price
 						</option>
-						<option value=''>All</option>
-						<option value='0, 20'>$0-$20</option>
-						<option value='21, 50'>$21-$50</option>
-						<option value='51, 80'>$51-$80</option>
-						<option value='81, 100'>$81-$100</option>
-						<option value='101, 1000000'> > $100~</option>
-					</select>
-				</div>
-				<div className='search-field'>
-					{/* <label>Sort results by</label> */}
-					<select
-						name='orderBy'
-						placeholder='Order By'
-						onChange={handleChange}
-						value={fields.orderBy}
-						required>
-						<option value='' disabled hidden>
-							Sort by
+							<option value=''>All</option>
+							<option value='0, 20'>$0-$20</option>
+							<option value='21, 50'>$21-$50</option>
+							<option value='51, 80'>$51-$80</option>
+							<option value='81, 100'>$81-$100</option>
+							<option value='101, 1000000'> > $100~</option>
+						</select>
+					</div>
+					<div className='search-field'>
+						{/* <label>Sort results by</label> */}
+						<select
+							name='orderBy'
+							placeholder='Order By'
+							onChange={handleChange}
+							value={fields.orderBy}
+							required>
+							<option value='' disabled hidden>
+								Sort by
 						</option>
-						<option value='id_ASC'>None</option>
-						{/* <option value="asdf">Most reviews</option>
+							<option value='id_ASC'>None</option>
+							{/* <option value="asdf">Most reviews</option>
           <option value="fasdfasdf">Least reviews</option> */}
-						<option value='price_ASC'>Price, low to high</option>
-						<option value='price_DESC'>Price, high to low</option>
-						<option value='lastUpdated_DESC'>Newest</option>
-						<option value='lastUpdated_ASC'>Oldest</option>
-					</select>
+							<option value='price_ASC'>Price, low to high</option>
+							<option value='price_DESC'>Price, high to low</option>
+							<option value='lastUpdated_DESC'>Newest</option>
+							<option value='lastUpdated_ASC'>Oldest</option>
+						</select>
+					</div>
+
 				</div>
 				{/* <div className='search-field-bottom'> */}
-				<div className='search-field-keyword'>
-					{/* <label className='search-label-keywords'>Keywords</label> */}
-					<input
-						className='search-by-keyword-input'
-						type='text'
-						name='tags'
-						value={fields.tags}
-						onChange={handleChange}
-						placeholder={`Search by Keyword`}
-						onKeyDown={handlePress}
-					/>
+				<div className='search-dropdowns'>
+					<div className='search-field-keyword'>
+						{/* <label className='search-label-keywords'>Keywords</label> */}
+						<input
+							className='search-by-keyword-input'
+							type='text'
+							name='tags'
+							value={fields.tags}
+							onChange={handleChange}
+							placeholder={`Search by Keyword`}
+							onKeyDown={handlePress}
+						/>
 
-					{/* <div className='search-buttons'> */}
-					<button className='search-apply' onClick={e => handleSubmit(e)}>
-						{search()}
-					</button>
-				</div>
-				<button className='search-reset' onClick={handleReset}>
-					Reset
+						{/* <div className='search-buttons'> */}
+						<button className='search-apply' onClick={e => handleSubmit(e)}>
+							{search()}
+						</button>
+					</div>
+					<button className='search-reset' onClick={handleReset}>
+						Reset
 				</button>
+
+				</div>
 				{/* </div> */}
 				{/* </div> */}
 				{/* <div className="search-tags-container">
