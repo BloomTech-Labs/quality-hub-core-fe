a3b3074fddf470f453d0a553672d05380a76f70f
Jaytee <Padilla.Jaytee@gmail.com>
2020-01-28 15:00:02 -0800

    Added comments to code for clarity

--- src/InterviewQ/components/CoachForm/subs/BottomButtons.js
+++ src/InterviewQ/components/CoachForm/subs/BottomButtons.js
@@ -5,6 +5,8 @@ const BottomButtons = ({ handleSave, handleSubmit, formState, setDone, setOpen, 
 	function requiredMet(e) {
 		(formState.company.length === 0 || formState.position.length === 0 || formState.description.length === 0) ? setRequiredState({...requiredState, any: true}) : handleSubmit(e, formState, setDone, setOpen, addPost);
 
+		// Upon clicking the publish button for a coach listing, the entire page will refresh. This allows the settings button in the LeftNav of the /interviewq route to appear.
+		// In a future feature, Auth0 will be implemented and some state management overhauling will be done, so this page refresh will be removed and the re-rendering of the settings button will be controlled by state
 		window.location.reload();
 	}
 
