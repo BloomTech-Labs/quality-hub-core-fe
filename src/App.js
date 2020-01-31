a6147c03a102a36c2d4487e060d1135989bdff3a
Hugo Oliveira <oliveira.hugo.c@gmail.com>
2020-01-31 17:52:30 -0600

    Implemented auth0 redirecting new users. Need to combine auth0 and form data to create new user

--- src/App.js
+++ src/App.js
@@ -1,30 +1,44 @@
-import React, { useState } from 'react';
-import { Route, useLocation } from 'react-router-dom';
-import './global/styles/index.scss';
-import NavBar from './global/components/NavBar';
-import InterviewQ from './global/routes/InterviewQ';
-import Core from './global/routes/Core';
+import React, { useState } from "react";
+import { Route, useLocation } from "react-router-dom";
+import "./global/styles/index.scss";
+import NavBar from "./global/components/NavBar";
+import InterviewQ from "./global/routes/InterviewQ";
+import Core from "./global/routes/Core";
 
 function App() {
-	const [loggedin, setLoggedin] = useState(false);
-	let { pathname } = useLocation();
+  const [loggedin, setLoggedin] = useState(false);
+  let { pathname } = useLocation();
 
-	return (
-		<div className='App'>
-			{!pathname.includes('/meeting') && (
-				<Route
-					path='/'
-					render={props => (
-						<NavBar {...props} loggedin={loggedin} setLoggedin={setLoggedin} />
-					)}
-				/>
-			)}
-			<div className='not-nav'>
-				<Core loggedin={loggedin} setLoggedin={setLoggedin} />
-				<InterviewQ loggedin={loggedin} setLoggedin={setLoggedin} />
-			</div>
-		</div>
-	);
+  
+  console.log("LOGIN COUNT FROM APP.JS", localStorage.getItem("loginCount"));
+  const loginCount = localStorage.getItem("loginCount");
+
+  if (loginCount === "1") {
+    //GO HERE ON SIGN UP
+    console.log("REDIRECTING FROM AUTH0 SIGN UP");
+    if (window.location.pathname != "/signup") {
+      window.location.pathname = "/signup";
+    }
+  } else {
+    console.log("NOT REDIRECTING");
+  }
+
+  return (
+    <div className="App">
+      {!pathname.includes("/meeting") && (
+        <Route
+          path="/"
+          render={props => (
+            <NavBar {...props} loggedin={loggedin} setLoggedin={setLoggedin} />
+          )}
+        />
+      )}
+      <div className="not-nav">
+        <Core loggedin={loggedin} setLoggedin={setLoggedin} />
+        <InterviewQ loggedin={loggedin} setLoggedin={setLoggedin} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/global/components/Auth/Auth.js
+++ src/global/components/Auth/Auth.js
@@ -1,17 +1,17 @@
-import auth0 from 'auth0-js';
+import auth0 from "auth0-js";
 
 class Auth {
   constructor() {
     this.auth0 = new auth0.WebAuth({
       domain: process.env.REACT_APP_AUTH0_DOMAIN,
       clientID: process.env.REACT_APP_CLIENT_ID,
-      redirectUri: 'http://localhost:3000/callback',
-      audience: 'https://explorequality.auth0.com/userinfo',
-      responseType: 'token id_token',
-      scope: 'openid email',
+      redirectUri: "http://localhost:3000/callback",
+      audience: "https://explorequality.auth0.com/userinfo",
+      responseType: "token id_token",
+      scope: "openid email"
     });
 
-    this.authFlag = 'isLoggedIn';
+    this.authFlag = "isLoggedIn";
 
     this.login = this.login.bind(this);
     this.logout = this.logout.bind(this);
@@ -39,8 +39,8 @@ class Auth {
   signOut() {
     localStorage.setItem(this.authFlag, JSON.stringify(false));
     this.auth0.logout({
-      returnTo: 'http://localhost:3000',
-      clientID: 'LUft9iOEONnQilP8mFDdmiBHdNljGJ2u',
+      returnTo: "http://localhost:3000",
+      clientID: "LUft9iOEONnQilP8mFDdmiBHdNljGJ2u"
     });
   }
 
@@ -50,15 +50,32 @@ class Auth {
 
   handleAuthentication() {
     this.auth0.parseHash((err, authResults) => {
-      console.log(authResults);
+      console.log("AUTH RESULTS", authResults);
       if (authResults && authResults.idToken) {
         let expiresAt = JSON.stringify(
-          authResults.expiresIn * 1000 + new Date().getTime()
+          authResults.expiresIn * 1 + new Date().getTime()
         );
         // localStorage.setItem("access_token", authResults.accessToken);
         localStorage.setItem("token", authResults.idToken);
+        localStorage.setItem("email", authResults.idTokenPayload.email);
+
+        localStorage.setItem(
+          "loginCount",
+          authResults.idTokenPayload["http://logins"]
+        );
+
         localStorage.setItem("expires_at", expiresAt);
         localStorage.setItem("id", authResults.idTokenPayload.sub);
+
+        // if (localStorage.getItem("loginCount") > 1) {
+        //   //GO HERE ON SIGN IN
+        //   console.log("REDIRECTING FROM AUTH0 SIGN IN");
+        //   this.props.history.replace("/");
+        // } else {
+        //   //GO HERE ON SIGN UP
+        //   console.log("REDIRECTING FROM AUTH0 SIGN UP");
+        //   this.props.history.replace("/signup");
+        // }
       } else {
         console.log(err);
       }
@@ -66,6 +83,7 @@ class Auth {
   }
 
   setSession(authResult) {
+    console.log("AUTH RESULT", authResult);
     this.idToken = authResult.idToken;
     console.log("setSession()", this.idToken);
     // set the time that the id token will expire at
@@ -74,13 +92,13 @@ class Auth {
 
   logout() {
     this.auth0.logout({
-      returnTo: 'http://localhost:3000',
-      clientID: process.env.REACT_APP_CLIENT_ID,
+      returnTo: "http://localhost:3000",
+      clientID: process.env.REACT_APP_CLIENT_ID
     });
   }
 
   silentAuth() {
-    if(this.isAuthenticated()) {
+    if (this.isAuthenticated()) {
       return new Promise((resolve, reject) => {
         this.auth0.checkSession({}, (err, authResult) => {
           if (err) {
