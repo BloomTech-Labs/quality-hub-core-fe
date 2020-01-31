542b4168224816cb1160c316d8e022caafc35ffe
Jnmendza <jonatmendoza@gmail.com>
2020-01-31 12:58:41 -0800

    completedResumeReviewsBySeeker is being passed with the right data into components that are in the seekerpanel history tab

--- src/InterviewQ/components/Review/ReviewPage.js
+++ src/InterviewQ/components/Review/ReviewPage.js
@@ -3,19 +3,18 @@ import React, { useState, useEffect } from 'react';
 import ReviewForm from './subs/1_ReviewForm';
 import Modal from './subs/3_Modal';
 
-import './RQReviewPage.scss';
-import styles from './subs/RQModal.module.scss';
+import './ReviewPage.scss';
+import styles from './subs/Modal.module.scss';
 
 // TODO move comments to global component
 // TODO: add state for message relating to service.. 
 // * Important
 
 const ReviewPage = (props) => {
-
   // Refreshing causese state to be undefined, so we go back a page
-  // if (!props.location.state) {
-  //   props.history.push('/resumeq/seekerpanel');
-  // }
+  if (!props.location.state) {
+    props.history.goBack();
+  }
 
   //false sets the default to not show the Done modal
   const [open, setOpen] = useState(false);
@@ -23,13 +22,9 @@ const ReviewPage = (props) => {
   const closeWindow = () => {
     setOpen(true)
     setTimeout(() => {
-      props.history.push('/resumeq/seekerpanel');
+      props.history.push('/interviewq/history');
     }, 200)
   }
-
-  // if (window.location.includes('rating')) {
-  //   alert(window.location)
-  // }
 
   //This sets the darkened overlay behind the modals
   useEffect(() => {
@@ -44,12 +39,11 @@ const ReviewPage = (props) => {
     <div>
       <div id='iq-review-page' className={styles.overlay} onClick={closeWindow}></div>
       {open && <Modal closeWindow={closeWindow} />}
-      <div className='RQreview-page'>
+      <div className='review-page'>
         <h2>Rating & Review</h2>
         <hr />
         <p>Your review will help other job seekers find the best coach.</p>
-        {console.log('props in reviewPage', props)}
-        <ReviewForm location={props.location} history={props.history} setOpen={setOpen}/>
+        <ReviewForm location={props.location} history={props.history} id={props.match.params.id} setOpen={setOpen} />
       </div>
     </div>
   )
