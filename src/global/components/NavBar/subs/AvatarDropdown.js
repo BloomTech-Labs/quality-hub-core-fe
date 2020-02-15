892190e75b05667df4c984d2c327c4089df1214b
Bernie Durfee <berniedurfee@users.noreply.github.com>
2020-02-14 21:53:44 -0500

    Carved out unnecessary auth code

--- src/global/components/NavBar/subs/AvatarDropdown.js
+++ src/global/components/NavBar/subs/AvatarDropdown.js
@@ -6,9 +6,6 @@ import { useLazyQuery } from "@apollo/react-hooks";
 import { Gear } from "../../../icons/gear";
 import { Signout } from "../../../icons/signout";
 
-// auth0
-import auth from "../../../../global/components/Auth/Auth";
-
 // Icons
 import { blankavatar } from "../../../icons/blankavatar";
 
@@ -37,7 +34,7 @@ const AvatarDropdown = props => {
     document.removeEventListener("mousedown", handleOutsideClick);
     setOpen(false);
     props.logout();
-    auth.logout();
+    // auth.logout();
   };
 
   //If you click outside the dropdown menu, the menu will close.
