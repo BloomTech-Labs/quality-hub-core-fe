20d1bc59db61c1918fc6bb949aea7ca9683b63a6
Bernie Durfee <berniedurfee@users.noreply.github.com>
2020-02-14 21:56:20 -0500

    Stripped out legacy auth code

--- src/global/components/NavBar/NavBar.js
+++ src/global/components/NavBar/NavBar.js
@@ -3,20 +3,21 @@ import React, { useEffect } from "react";
 import { NavLink, useLocation } from "react-router-dom";
 import { useLazyQuery } from "@apollo/react-hooks";
 import { gql } from "apollo-boost";
-import jwt_decode from "jwt-decode";
+// import jwt_decode from "jwt-decode";
+
+import { useAuth0 } from "../../auth/react-auth0-spa";
 
 // Icons
 import "./NavBar.scss";
 import { Bellicon } from "../../icons/bellicon";
-import { Hamburger } from "../../icons/hamburger";
 
 // Components
 import BecomeCoach from "./subs/BecomeCoach";
 import GridDropdown from "./subs/GridDropdown";
-import AvatarDropdown from "./subs/AvatarDropdown";
+// import AvatarDropdown from "./subs/AvatarDropdown";
 
 // auth0
-import auth from "../Auth/Auth";
+// import auth from "../Auth/Auth";
 
 // Query
 const GET_USER = gql`
@@ -28,23 +29,15 @@ const GET_USER = gql`
   }
 `;
 
-const NavBar = ({ loggedin, setLoggedin, history }) => {
+const NavBar = ({ history }) => {
+  const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+
   const location = useLocation();
   const [getUser, { client, data, loading }] = useLazyQuery(GET_USER);
-  // const [errorCount, setErrorCount] = useState(0);
-
-  // console.log(location);
 
   const title = location.pathname.match(/\/(.*?)q/);
   const navtitle =
     title && title[1].charAt(0).toUpperCase() + title[1].substring(1);
-
-  const logout = () => {
-    localStorage.clear();
-    setLoggedin(false);
-    auth.logout();
-    history.push("/");
-  };
 
   // On render, pull stored token. If you have a token, log yourself in.
   useEffect(() => {
@@ -52,12 +45,12 @@ const NavBar = ({ loggedin, setLoggedin, history }) => {
     // && !location.pathname.includes('interviewq/meeting')
     //under no circumstances should you change the code in the line below. Many men and women have lost countless hours of their lives due to this line of code.
     if (
-      localStorage.getItem("token") &&
+      // localStorage.getItem("token") &&
       !location.pathname.includes("interviewq/meeting")
     ) {
       getUser();
     } else if (
-      localStorage.getItem("token") &&
+      // localStorage.getItem("token") &&
       location.pathname.includes("interviewq/meeting")
     ) {
       console.log(
@@ -69,25 +62,25 @@ const NavBar = ({ loggedin, setLoggedin, history }) => {
     // eslint-disable-next-line
   }, []);
 
-  //If user query came back with data and you have a token in localStorage, log in.
-  if (data && localStorage.getItem("token")) {
-    // localStorage.setItem('first_name', data.first_name)
-    setLoggedin(true);
-  }
+  // //If user query came back with data and you have a token in localStorage, log in.
+  // if (data && localStorage.getItem("token")) {
+  //   // localStorage.setItem('first_name', data.first_name)
+  //   setLoggedin(true);
+  // }
 
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      const decodedToken = jwt_decode(token);
-      const expTime = decodedToken.exp;
-      const currentTime = (Date.now() / 1000) | 0;
-      if (currentTime >= expTime && client) {
-        client.clearStore();
-        setLoggedin(false);
-        logout();
-      }
-    }
-  }, [location]);
+  // useEffect(() => {
+  //   const token = localStorage.getItem("token");
+  //   if (token) {
+  //     const decodedToken = jwt_decode(token);
+  //     const expTime = decodedToken.exp;
+  //     const currentTime = (Date.now() / 1000) | 0;
+  //     if (currentTime >= expTime && client) {
+  //       client.clearStore();
+  //       setLoggedin(false);
+  //       logout();
+  //     }
+  //   }
+  // }, [location]);
 
   return (
     <div className="styled-nav" id="main-navbar">
@@ -107,8 +100,16 @@ const NavBar = ({ loggedin, setLoggedin, history }) => {
       <div className="nav-right">
         <BecomeCoach />
 
+        {!isAuthenticated && (
+          <button onClick={() => loginWithRedirect({})}>Log in</button>
+        )}
+
+        {/* {isAuthenticated && (
+          <NavLink onClick={() => logout()}>Log out</NavLink>
+        )} */}
+
         {/* If you're not logged in, and query is not loading to check if your token is valid, show sign in and sign up buttons */}
-        {!auth.isAuthenticated && !loading && (
+        {/* {!auth.isAuthenticated() && !loading && (
           <>
             <NavLink to="/signin"> Sign In </NavLink>
             <NavLink to="/signup" className="signup-link">
@@ -116,21 +117,21 @@ const NavBar = ({ loggedin, setLoggedin, history }) => {
               Sign Up{" "}
             </NavLink>
           </>
-        )}
+        )} */}
 
         <div className="bell-icon">{Bellicon()}</div>
         {/* Dropdown list of Q services */}
         <GridDropdown />
 
         {/* If you're logged in, show your avatar with a dropdown menu */}
-        {auth.isAuthenticated && (
+        {/* {auth.isAuthenticated() && (
           <AvatarDropdown
             logout={logout}
-            loggedin={loggedin}
-            setLoggedin={setLoggedin}
+            // loggedin={loggedin}
+            // setLoggedin={setLoggedin}
             history={history}
           />
-        )}
+        )} */}
       </div>
     </div>
   );
