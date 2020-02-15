301edebbde3b893656cf5368685322a6a5ef665e
Bernie Durfee <berniedurfee@users.noreply.github.com>
2020-02-14 21:54:33 -0500

    Moved Apollo code into App component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,73 @@
 import React, { useState } from "react";
 import { Route, useLocation } from "react-router-dom";
+
 import "./global/styles/index.scss";
 import NavBar from "./global/components/NavBar";
 import InterviewQ from "./global/routes/InterviewQ";
 import Core from "./global/routes/Core";
+import { useAuth0 } from "./global/auth/react-auth0-spa";
+
+import { ApolloProvider } from "@apollo/react-hooks";
+import { ApolloClient, HttpLink, InMemoryCache } from "apollo-boost";
+import { setContext } from "apollo-link-context";
 
 function App() {
-  const [loggedin, setLoggedin] = useState(false);
   let { pathname } = useLocation();
 
-  
+  const [accessToken, setAccessToken] = useState("");
+
+  const { loading, getTokenSilently, isAuthenticated, user } = useAuth0();
+  if (loading) {
+    return "Loading...";
+  }
+
+  // get access token
+  const getAccessToken = async () => {
+    // getTokenSilently() returns a promise
+    try {
+      const token = await getTokenSilently();
+      setAccessToken(token);
+      console.log("Token: ", token);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  getAccessToken();
+
+  const httpLink = new HttpLink({
+    uri: "https://quality-hub-core-staging.herokuapp.com"
+  });
+
+  const authLink = setContext((_, { headers }) => {
+    const token = accessToken;
+    if (token) {
+      return {
+        headers: {
+          ...headers,
+          authorization: `Bearer ${token}`
+        }
+      };
+    } else {
+      return {
+        headers: {
+          ...headers
+        }
+      };
+    }
+  });
+
+  const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache()
+  });
+
   console.log("LOGIN COUNT FROM APP.JS", localStorage.getItem("loginCount"));
   const loginCount = localStorage.getItem("loginCount");
 
   if (loginCount === "1") {
     //GO HERE ON SIGN UP
     console.log("REDIRECTING FROM AUTH0 SIGN UP");
-    if (window.location.pathname != "/signup") {
+    if (window.location.pathname !== "/signup") {
       window.location.pathname = "/signup";
     }
   } else {
@@ -24,20 +75,17 @@ function App() {
   }
 
   return (
-    <div className="App">
-      {!pathname.includes("/meeting") && (
-        <Route
-          path="/"
-          render={props => (
-            <NavBar {...props} loggedin={loggedin} setLoggedin={setLoggedin} />
-          )}
-        />
-      )}
-      <div className="not-nav">
-        <Core loggedin={loggedin} setLoggedin={setLoggedin} />
-        <InterviewQ loggedin={loggedin} setLoggedin={setLoggedin} />
+    <ApolloProvider client={client}>
+      <div className="App">
+        {!pathname.includes("/meeting") && (
+          <Route path="/" render={props => <NavBar {...props} />} />
+        )}
+        <div className="not-nav">
+          <Core />
+          <InterviewQ />
+        </div>
       </div>
-    </div>
+    </ApolloProvider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,53 +1,43 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createBrowserHistory } from "history";
+
 import "./index.scss";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
-import ApolloClient, { InMemoryCache } from "apollo-boost";
-import { ApolloProvider } from "@apollo/react-hooks";
-import { StripeProvider } from "react-stripe-elements";
-// auth0
-import auth from "./global/components/Auth/Auth";
-require("dotenv").config();
 
-const addBearerToToken = token => {
-  return token ? `Bearer ${token}` : "";
-};
+import { Auth0Provider } from "./global/auth/react-auth0-spa";
+import { StripeProvider } from "react-stripe-elements";
+
+require("dotenv").config();
 
 const stripeKey = process.env.REACT_APP_STRIPE_KEY || "stripe";
 
-const cache = new InMemoryCache();
-
-const client = new ApolloClient({
-  //https://quality-hub-gateway-staging.herokuapp.com/
-  // uri: 'https://quality-hub-gateway-staging.herokuapp.com/',
-  uri: "https://quality-hub-core-staging.herokuapp.com/",
-  request: operation => {
-    operation.setContext(context => ({
-      headers: {
-        ...context.headers,
-        Authorization: addBearerToToken(auth.getIdToken())
-      }
-    }));
-  },
-  cache,
-  resolvers: {}
-});
-
-cache.writeData({
-  data: {
-    isLoggedIn: !!localStorage.getItem("token"), // Logic needs update
-    isCoach: false // Needs update
-  }
-});
+const onRedirectCallback = appState => {
+  createBrowserHistory().push(
+    appState && appState.targetUrl
+      ? appState.targetUrl
+      : window.location.pathname
+  );
+};
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
+  <Auth0Provider
+    domain={process.env.REACT_APP_AUTH0_DOMAIN}
+    client_id={process.env.REACT_APP_CLIENT_ID}
+    redirect_uri="http://localhost:3000/callback"
+    onRedirectCallback={onRedirectCallback}
+    redirectUri="http://localhost:3000/callback"
+    audience="https://explorequality.auth0.com/userinfo"
+    responseType="token id_token"
+    scope="openid email"
+  >
     <StripeProvider apiKey={stripeKey}>
       <Router>
         <App />
       </Router>
     </StripeProvider>
-  </ApolloProvider>,
+  </Auth0Provider>,
+
   document.getElementById("root")
 );
